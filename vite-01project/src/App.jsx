8c589d297674f1d57882a292fe8a7e116f825ec8
siddhant-bhatnagar <siddhantbhatnagar11@gmail.com>
2026-04-08 22:51:35 +0530

    feat: initialize Vite project with React, add styles and SVG icons
    - Created a new Vite project with React setup.
    - Added App component with basic structure and images.
    - Included CSS styles for layout and theming.
    - Added SVG icons for various functionalities.
    - Configured Vite for React support.

---
+++ vite-01project/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <p>krish</p>
+      <img src={reactLogo} alt="react" width="100" />
+      <img src={viteLogo} alt="vite" width="100" />
+      <img src={heroImg} alt="hero" width="200" />
+    </div>
+  )
+}
+
+export default App
