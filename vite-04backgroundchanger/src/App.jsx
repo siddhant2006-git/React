3c7c3b5b7b500cd5f01a3b0a6aed8eb41ac45089
siddhant-bhatnagar <siddhantbhatnagar11@gmail.com>
2026-04-27 22:59:25 +0530

    bg color changer

--- vite-04backgroundchanger/src/App.jsx
+++ vite-04backgroundchanger/src/App.jsx
@@ -1,17 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState("yellow");
 
   return (
-    <>
-      <div className=''></div>
-    </>
+    <div className="flex items-center justify-center h-screen" style={{ backgroundColor: color }}>
+      
+      <div className="flex gap-4 mt-4 justify-center bottom-12 inset-x-0
+      px-2">
+        test
+
+      </div>
+      
+      <button className="outline-none px-2"
+        onClick={() => setColor("red")}
+        style={{ background: "red" }} >red</button>
+      
+      
+      <button className="outline-none px-2"
+        onClick={() => setColor("green")}
+        style={{ background: "green" }} >green</button>
+      
+      
+      <button
+        className="outline-none px-2"
+        onClick={() => setColor("blue")}
+        style={{ background: "blue" }} >blue</button>
+      
+      
+      <button className="outline-none px-2"
+        onClick={() => setColor("yellow")}
+        style={{ background: "yellow" }} >yellow</button>
+      
+      
+    </div>
   )
 }
 
-export default App
+export default App;
