28d5fb600c1a130a80138f46dcfb377ed50b68b9
siddhant-bhatnagar <siddhantbhatnagar11@gmail.com>
2026-04-28 22:00:25 +0530

    feat: initialize password generator project with Vite and React
    - Add main application structure with App component and entry point.
    - Include CSS styles for layout and components.
    - Create SVG icons for social media and documentation.
    - Set up Vite configuration for React.
    - Add assets including hero image and logos.

--- vite-04backgroundchanger/src/App.jsx
+++ vite-04backgroundchanger/src/App.jsx
@@ -1,40 +1,57 @@
 import { useState } from "react";
 
 function App() {
-  const [color, setColor] = useState("yellow");
+  const [color, setColor] = useState("olive");
 
   return (
-    <div className="flex items-center justify-center h-screen" style={{ backgroundColor: color }}>
-      
-      <div className="flex gap-4 mt-4 justify-center bottom-12 inset-x-0
-      px-2">
+    <div
+      className="flex items-center justify-center h-screen"
+      style={{
+        backgroundColor: color,
+        minHeight: "100vh",
+        width: "100%",
+       }}
+    >
+      <div
+        className="flex gap-4 mt-4 justify-center bottom-12 inset-x-0
+      px-2 "
+      >
         test
-
       </div>
-      
-      <button className="outline-none px-2"
+
+      <button
+        className="outline-none px-2"
         onClick={() => setColor("red")}
-        style={{ background: "red" }} >red</button>
-      
-      
-      <button className="outline-none px-2"
+        style={{ background: "red" }}
+      >
+        red
+      </button>
+
+      <button
+        className="outline-none px-2"
         onClick={() => setColor("green")}
-        style={{ background: "green" }} >green</button>
-      
-      
+        style={{ background: "green" }}
+      >
+        green
+      </button>
+
       <button
         className="outline-none px-2"
         onClick={() => setColor("blue")}
-        style={{ background: "blue" }} >blue</button>
-      
-      
-      <button className="outline-none px-2"
+        style={{ background: "blue" }}
+      >
+        blue
+      </button>
+
+      <button
+        className="outline-none px-2"
         onClick={() => setColor("yellow")}
-        style={{ background: "yellow" }} >yellow</button>
-      
-      
+        style={{ background: "yellow" }}
+      >
+        yellow
+      </button>
     </div>
-  )
+  );
 }
 
 export default App;
