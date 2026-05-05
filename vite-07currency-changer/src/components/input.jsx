bd5c38f0d02d200768ebc3c928dd5d3f7fb65074
siddhant-bhatnagar <siddhantbhatnagar11@gmail.com>
2026-05-05 22:22:05 +0530

    feat: refactor App and InputBox components for currency conversion functionality

--- vite-07currency-changer/src/components/input.jsx
+++ vite-07currency-changer/src/components/input.jsx
@@ -1,47 +1,60 @@
-import { useState } from "react";
+import React, { useId } from "react";
 
-function InputBox({ onAddCrypto }) {
-  const [cryptoName, setCryptoName] = useState("");
-  const [cryptoPrice, setCryptoPrice] = useState("");
-
-  function handleSubmit(e) {
-    e.preventDefault();
-
-    if (cryptoName === "" || cryptoPrice === "") {
-      alert("Please enter crypto name and price");
-      return;
-    }
-
-    const newCrypto = {
-      id: Date.now(),
-      name: cryptoName,
-      price: cryptoPrice,
-    };
-
-    onAddCrypto(newCrypto);
-
-    setCryptoName("");
-    setCryptoPrice("");
-  }
+function InputBox({
+  label,
+  amount,
+  onAmountChange,
+  onCurrencyChange,
+  currencyOptions = [],
+  selectCurrency = "usd",
+  amountDisable = false,
+  currencyDisable = false,
+  className = "",
+}) {
+  const amountInputId = useId();
 
   return (
-    <form className="form" onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Enter crypto name e.g. Bitcoin"
-        value={cryptoName}
-        onChange={(e) => setCryptoName(e.target.value)}
-      />
+    <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
+      <div className="w-1/2">
+        <label
+          htmlFor={amountInputId}
+          className="text-black/40 mb-2 inline-block"
+        >
+          {label}
+        </label>
 
-      <input
-        type="number"
-        placeholder="Enter price e.g. 65000"
-        value={cryptoPrice}
-        onChange={(e) => setCryptoPrice(e.target.value)}
-      />
+        <input
+          id={amountInputId}
+          className="outline-none w-full bg-transparent py-1.5 text-black"
+          type="number"
+          placeholder="Amount"
+          disabled={amountDisable}
+          value={amount}
+          onChange={(e) =>
+            onAmountChange && onAmountChange(Number(e.target.value))
+          }
+        />
+      </div>
 
-      <button type="submit">Add Crypto</button>
-    </form>
+      <div className="w-1/2 flex flex-wrap justify-end text-right">
+        <p className="text-black/40 mb-2 w-full">Currency Type</p>
+
+        <select
+          className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none text-black"
+          value={selectCurrency}
+          onChange={(e) =>
+            onCurrencyChange && onCurrencyChange(e.target.value)
+          }
+          disabled={currencyDisable}
+        >
+          {currencyOptions.map((currency) => (
+            <option key={currency} value={currency}>
+              {currency}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
   );
 }
 
