d0f3044433911e72b5d0d8baa452d4b55e05fc81
siddhant-bhatnagar <siddhantbhatnagar11@gmail.com>
2026-05-07 22:56:52 +0530

    React-router

--- vite-08React-router/src/main.jsx
+++ vite-08React-router/src/main.jsx
@@ -1,10 +1,39 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import Footer from './components/footer/footer.jsx'
+import Header from './components/header/Header.jsx'
+import About from './components/about/about.jsx'
+import Contact from './components/contact/contact.jsx'
+
+
+const router = createBrowserRouter([
+  {
+    path: `/`,
+    element: <Header />
+
+  },
+  {
+    path: "Footer",
+    element: <Footer />
+  },
+  {
+    path: "about",
+    element: <About />
+  },
+  {
+    path: "contact",
+    element:<Contact/>
+
+  }
+])
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
+
---
+++ vite-08React-router/src/root.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Footer from './components/footer/footer'
+import Header from './components/header/Header'
+// Outlet- is used to change the value between the header , inside the header 
+
+function root() {
+  return (
+    <>
+      <Header />
+      <Outlet/>
+      <Footer />
+  
+      
+    </>
+  )
+}
+
+export default root
