f9bd3404d3f2728aea349aa1fcc2edfbc34cebfb
siddhant-2006 <siddhantbhatnagar11@gmail.com>
2026-05-18 21:47:42 +0530

    feat: initialize Vite project with React and Tailwind CSS
    - Added hero image and SVG icons for UI components.
    - Created main entry point for React application in main.jsx.
    - Configured Vite with React and Tailwind CSS plugins in vite.config.js.
    - Imported Tailwind CSS in index.css for styling.

---
+++ vite-12grifClone/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import Home from './components/layers/Home'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      const router=createBrowserRouter([
+      {
+        Element:<
+      
+      }
+    ])
+  </div>
+  )
+}
+
+export default App
